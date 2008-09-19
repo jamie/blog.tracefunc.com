88d57a1e2665ea91151fa42715b012284f44f0ab
Jamie Macey <jamie.git@tracefunc.com>
2008-09-18 22:03:58 -0400

    fix hAtom feed

--- lib/static_helper.rb
+++ lib/static_helper.rb
@@ -6,6 +6,15 @@ module StaticHelper
   def js(file)
     %(<script type='text/javascript' src="/js/#{file}.js"></script>)
   end
+  
+  def atom_datestamp(date)
+    date = DateTime.parse(date) unless date.kind_of? DateTime
+    short = date.strftime('%Y-%m-%dT%H:%M:%S%Z')
+    long = date.strftime('%b %e, %Y at %H:%M')
+    %|<abbr class="published date" title="#{short}">#{long}</abbr>|
+  rescue
+    "<abbr>draft</abbr>"
+  end
 end
 
 Webby::Helpers.register(StaticHelper)
