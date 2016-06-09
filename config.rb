df71adc4985132d5204c3925fcfaa4a5a3e8aa48
Jamie Macey <jamie.git@tracefunc.com>
2016-06-08 18:12:04 -0700

    move articles into sources dir, rewrite matcher for subdir

--- config.rb
+++ config.rb
@@ -33,7 +33,7 @@ activate :blog do |blog|
 
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.sources = "articles/{year}-{month}-{day}-{title}"
   # blog.taglink = "tags/{tag}.html"
   # blog.layout = "layout"
   # blog.summary_separator = /(READMORE)/
