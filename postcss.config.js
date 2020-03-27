b1021dff2a6b277d37862a701434835f7cf0467a
Jamie Macey <jamie.git@tracefunc.com>
2020-03-26 19:51:47 -0700

    Trying to get Purgecss working to reduce css file size

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,26 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g) || []
+  }
+}
+
 module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
+    // purgecss({
+    //   content: [
+    //     __dirname + '/source/**/*.html',
+    //     __dirname + '/source/**/*.erb'
+    //   ],
+    //   extractors: [{
+    //     extractor: TailwindExtractor,
+    //     extensions: ['html', 'erb', 'html.erb']
+    //   }],
+    //   css: ['.tmp/dist/*.css'],
+    // }),
   ]
 }
