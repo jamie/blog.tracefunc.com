e0106abb9a03fc2d329d7742b3f23754d273c6c4
Jamie Macey <jamie.git@tracefunc.com>
2020-03-26 19:50:51 -0700

    Not needed anymore

--- config.rb
+++ config.rb
@@ -16,8 +16,6 @@ page '/*.txt', layout: false
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
-ignore "/article.html.erb"
-
 # General configuration
 
 # Reload the browser automatically whenever files change
