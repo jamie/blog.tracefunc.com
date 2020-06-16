c4108e949c9c3303e67216adf9df313199b1f29d
Jamie Macey <jamie.git@tracefunc.com>
2020-06-16 11:25:17 -0700

    WIP migration

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
+const inProduction = (process.env.NODE_ENV == 'production');
 
 class TailwindExtractor {
   static extract(content) {
@@ -6,21 +6,28 @@ class TailwindExtractor {
   }
 }
 
+const DEV_PLUGINS = [
+  require('postcss-import')(),
+  require('tailwindcss')(),
+  require('autoprefixer')()
+]
+const PROD_PLUGINS = [
+  require('postcss-import')(),
+  require('tailwindcss')(),
+  require('autoprefixer')(),
+  require('postcss-purgecss')({
+    content: [
+      __dirname + '/../source/**/*.html',
+      __dirname + '/../source/**/*.erb'
+    ],
+    extractors: [{
+      extractor: TailwindExtractor,
+      extensions: ['html', 'js', 'erb', 'html.erb']
+    }]
+  }),
+  require('cssnano')()
+]
+
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-    // purgecss({
-    //   content: [
-    //     __dirname + '/source/**/*.html',
-    //     __dirname + '/source/**/*.erb'
-    //   ],
-    //   extractors: [{
-    //     extractor: TailwindExtractor,
-    //     extensions: ['html', 'erb', 'html.erb']
-    //   }],
-    //   css: ['.tmp/dist/*.css'],
-    // }),
-  ]
+  plugins: inProduction ? PROD_PLUGINS : DEV_PLUGINS
 }
