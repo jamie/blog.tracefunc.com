51e3963d2190c86fd8d56ea27dd87d09d4e0d462
Jamie Macey <jamie.git@tracefunc.com>
2020-07-25 22:43:10 -0700

    Extract remaining middleman customization

--- plugins/builders/notable.rb
+++ plugins/builders/notable.rb
@@ -27,3 +27,60 @@ class Notable < SiteBuilder
     config[:notable][:root]
   end
 end
+
+__END__
+
+
+class Wiki < Middleman::Extension
+  option :root, 'wiki', 'Root path for the wiki'
+  expose_to_template :wikilink!
+
+  def initialize(app, options_hash={}, &block)
+    super
+  end
+
+  def after_configuration
+    app.sitemap.resources.each do |r|
+      next unless r.path =~ %r{^#{options.root}/}
+      app.config_context.ignore r.path
+
+      title, *path = r.path.split('/').reverse
+      page = title.downcase.gsub(/ +/, '-')
+      full_path = [page, *path].reverse.join('/')
+
+      app.config_context.proxy "#{full_path}.html", r.path, directory_index: false
+    end
+  end
+
+  def manipulate_resource_list(resources)
+    resources.each do |r|
+      next unless r.path =~ %r{^#{options.root}/}
+
+      # General metadata
+      r.metadata[:options][:layout] = "notes"
+      r.metadata[:options][:content_type] = "text/html"
+
+      # Auto-generate some frontmatter
+      r.metadata[:page][:pagename] = r.path.split('/').last
+
+      if r.metadata.dig(:page, :title).blank?
+        title = r.path.split('/').last.gsub('_',' ').titleize
+        r.metadata[:page][:title] = title
+      end
+    end
+
+    resources
+  end
+
+  helpers do
+    def pages_linking_here
+      pagename = current_page.metadata.dig(:page, :pagename)
+      return [] unless pagename
+
+      sitemap.resources.sort_by(&:path).select do |r|
+        r.path =~ /notes/ && File.read(r.source_file) =~ /\[\[#{pagename}\]\]/i
+      end
+    end
+  end
+end
+::Middleman::Extensions.register(:wiki, Wiki)
