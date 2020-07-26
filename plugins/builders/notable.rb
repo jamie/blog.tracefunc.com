684ec4ef772c0123937ba3cc2e9524991499ee49
Jamie Macey <jamie.git@tracefunc.com>
2020-07-26 00:08:31 -0700

    Set up backlinks for notable pages

--- plugins/builders/notable.rb
+++ plugins/builders/notable.rb
@@ -2,85 +2,53 @@ class Notable < SiteBuilder
   LINK_PATTERN = %r{\[\[([^\]]+)\]\]}
   CONFIG_DEFAULTS = {
     notable: {
-      root: 'notes'
+      root: 'wiki'
     }
   }
-
-  def build
-    liquid_filter "wikilink", :wikilink
-  end
-  
-  def wikilink(content)
-    content ||= ""
-    content.gsub(LINK_PATTERN) do |match|
-      # For some reason I can't use last_match via the gsub call, so let's re-match
-      match =~ LINK_PATTERN
-
-      title = Regexp.last_match[1]
-      page = title.gsub(' ', '+')
-      href = [root, page].compact.join('/')
-      %(<a href="/#{href}/" class="wikilink">#{title}</a>)
-    end
-  end
 
   def root
     config[:notable][:root]
   end
-end
 
-__END__
-
-
-class Wiki < Middleman::Extension
-  option :root, 'wiki', 'Root path for the wiki'
-  expose_to_template :wikilink!
-
-  def initialize(app, options_hash={}, &block)
-    super
+  def build
+    generator :build_backlinks
+    generator :render_wikilinks
   end
 
-  def after_configuration
-    app.sitemap.resources.each do |r|
-      next unless r.path =~ %r{^#{options.root}/}
-      app.config_context.ignore r.path
-
-      title, *path = r.path.split('/').reverse
-      page = title.downcase.gsub(/ +/, '-')
-      full_path = [page, *path].reverse.join('/')
-
-      app.config_context.proxy "#{full_path}.html", r.path, directory_index: false
+  def build_backlinks
+    each_page do |page|
+      pagename = page.data[:title]
+      backlinks = site.pages.select {|pg| pg.content =~ /\[\[#{pagename}\]\]/i }
+      page.data[:backlinks] = backlinks if backlinks.any?
     end
   end
+  
+  def render_wikilinks
+    each_page do |page|
+      page.content.gsub!(LINK_PATTERN) do |match|
+        # For some reason I can't use last_match via the gsub call, so let's re-match
+        match =~ LINK_PATTERN
 
-  def manipulate_resource_list(resources)
-    resources.each do |r|
-      next unless r.path =~ %r{^#{options.root}/}
-
-      # General metadata
-      r.metadata[:options][:layout] = "notes"
-      r.metadata[:options][:content_type] = "text/html"
-
-      # Auto-generate some frontmatter
-      r.metadata[:page][:pagename] = r.path.split('/').last
-
-      if r.metadata.dig(:page, :title).blank?
-        title = r.path.split('/').last.gsub('_',' ').titleize
-        r.metadata[:page][:title] = title
-      end
-    end
-
-    resources
-  end
-
-  helpers do
-    def pages_linking_here
-      pagename = current_page.metadata.dig(:page, :pagename)
-      return [] unless pagename
-
-      sitemap.resources.sort_by(&:path).select do |r|
-        r.path =~ /notes/ && File.read(r.source_file) =~ /\[\[#{pagename}\]\]/i
+        title = Regexp.last_match[1]
+        link = site.pages.detect {|pg| pg.data[:title] == title}
+        if link
+          %(<a href="#{link.url}" class="wikilink">#{link.data[:title]}</a>)
+        else
+          title
+        end
       end
     end
   end
+
+private
+  def each_page
+    site.pages.each do |page|
+      next unless page.data[:categories] == 'notable'
+      yield page
+    end
+  end
+
+  def slugify(title)
+    title.downcase.gsub(/[^a-z0-9]+/, '-')
+  end
 end
-::Middleman::Extensions.register(:wiki, Wiki)
--- readme.md
+++ readme.md
@@ -11,3 +11,9 @@ This repo contains half-written and pre-publication articles, don't judge.
 ## Build/Publish
 
 - rake deploy (commits output folder to gh-pages branch)
+
+## TODO
+
+- Update old blog post names to include -html for url compatability
+- Update excerpt splitter (it renders in the page output)
+- Fix permalinks for notes
