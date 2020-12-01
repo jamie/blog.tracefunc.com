045523252712e8df42662d93f537c0dc716ff829
Jamie Macey <jamie.git@tracefunc.com>
2020-12-01 11:57:15 -0800

    Update build for GitHub actions [squash]
    update purgecss config
    fix debug output
    try a different binary?
    manual chrome install
    build/deploy Bridgetown manually
    syntax
    setup ruby & node by hand as well
    wtf a beta?
    find linux chrome, fall back to macos
    debug output
    pass token explicitly from secrets
    cleanup

--- plugins/html_to_pdf.rb
+++ plugins/html_to_pdf.rb
@@ -1,8 +1,9 @@
 class HtmlToPdf < SiteBuilder
   def build
     hook :site, :post_write do |site|
-      site.pages.select{ |page| page.data['print_pdf'] }.each do |page|
-        chrome = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+      site.pages.select { |page| page.data['print_pdf'] }.each do |page|
+        chrome = `which google-chrome`.chomp
+        chrome = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" if chrome.empty?
         system(
           chrome,
           "--headless",
