026174e8f55563004e36e4ff1f9d81c5ae7bfa01
Jamie Macey <jamie.git@tracefunc.com>
2020-12-07 12:02:26 -0800

    extract for clarity

--- plugins/html_to_pdf.rb
+++ plugins/html_to_pdf.rb
@@ -2,17 +2,22 @@ class HtmlToPdf < SiteBuilder
   def build
     hook :site, :post_write do |site|
       site.pages.select { |page| page.data['print_pdf'] }.each do |page|
-        chrome = `which google-chrome`.chomp
-        chrome = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" if chrome.empty?
         system(
           chrome,
-          "--headless",
-          "--disable-gpu",
-          "--no-margins",
+          '--headless', '--disable-gpu', '--no-margins',
           "--print-to-pdf=output#{page.data.print_pdf}",
           "output#{page.data.print_html}"
         )
       end
+    end
+  end
+
+  def chrome
+    @chrome ||= begin
+      [
+        `which google-chrome`.chomp,
+        '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+      ].reject(&:empty?).first
     end
   end
 end
