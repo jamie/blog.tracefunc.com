8e1b8156e4f1d11157731edb24f6d10bf0ced8ed
Jamie Macey <jamie.git@tracefunc.com>
2022-03-23 19:58:53 -0700

    Fixup CSS rendering and tailwind config

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,3 +1,6 @@
-import "../styles/index.scss"
+import "index.css"
+
+// Import all JavaScript & CSS files from src/_components
+import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
 
 console.info("Bridgetown is loaded!")
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 module.exports = {
-  purge: {
-    mode: "all",
-    content: [
-      "./frontend/**/*.css",
-      "./frontend/**/*.scss",
-      "./src/**/*.erb",
-      "./src/**/*.html",
-      "./src/**/*.liquid",
-      "./src/**/*.md",
-    ],
-  },
+  content: [
+    './frontend/**/*.{css,scss}',
+    './src/**/*.{html,js,erb,liquid,md}'
+  ],
   theme: {
-    extend: {},
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
-  variants: {},
-  plugins: [],
-};
+}
