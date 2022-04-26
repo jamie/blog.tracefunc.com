c2df5652f934799a1676ae2c0982ce0f1127081f
Jamie Macey <jamie.git@tracefunc.com>
2022-04-25 21:21:59 -0700

    Upgrade tailwind config for 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,39 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     standardFontWeights: true
   },
-  purge: {
-    content: [
-      "./output/**/*.html",
-      "./frontend/**/*.js",
-      "./src/**/*.md",
-      "./src/**/*.html",
-      "./src/**/*.liquid"
-    ]
-  },
+  content: [
+    "./output/**/*.html",
+    "./frontend/**/*.js",
+    "./src/**/*.md",
+    "./src/**/*.html",
+    "./src/**/*.liquid"
+  ],
   theme: {
-    extend: {}
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
-  variants: {},
   plugins: [
     // add this line if you use tailwindui
     // require('@tailwindcss/forms'),
